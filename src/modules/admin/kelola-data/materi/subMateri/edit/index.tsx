7fc553d5188aa1ab21ee650d99c1f979b5e78067
Abel Ananta <abelananta9@gmail.com>
2023-03-06 23:48:19 +0700

    fix: bug build

--- src/modules/admin/kelola-data/materi/subMateri/edit/index.tsx
+++ src/modules/admin/kelola-data/materi/subMateri/edit/index.tsx
@@ -68,6 +68,7 @@ export const EditSubMateri = () => {
         {formData.map((item, i) => (
           <EditSubMateriSection
             index={i}
+            key={i}
             deleteSlide={deleteSlide}
             storingTitle={storingTitle}
             storingContent={storingContent}
